a4fb956560cf0525a4189ba69b7dbb4840a6b405
g3-tou <munira.sangaaf@gmail.com>
2024-05-06 22:46:33 +1000

    first commit

---
+++ controller/ExploreTeamsController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import au.edu.uts.ap.javafx.ViewLoader;
+import au.edu.uts.ap.javafx.Controller;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import model.Player;
+import model.Players;
+import model.Team;
+import model.Teams;
+
+
+
+public class ExploreTeamsController extends Controller<Teams> {
+
+    @FXML private Button seeTeams;
+    @FXML private Button seePlayers;
+    @FXML private Button closeButton;
+    
+    @FXML private void close(ActionEvent event) {
+        stage.close();
+    }
+
+    public Teams getTeam(){
+        return model;
+    }
+
+    @FXML private void viewAllTeams(ActionEvent event) throws IOException{
+        Stage stage1 = new Stage();
+        stage1.setX(ViewLoader.X + 601);
+        stage1.setY(ViewLoader.Y);
+
+        stage1.getIcons().add(new Image("/view/nba.png"));
+        ViewLoader.showStage(getTeam(), "/view/TeamsTable.fxml", "Teams Menu", stage1);
+    }
+
+    @FXML private void viewAllPlayers(ActionEvent event) throws IOException{
+        Stage stage2 = new Stage();
+        stage2.setX(ViewLoader.X + 601);
+        stage2.setY(ViewLoader.Y);
+
+        stage2.getIcons().add(new Image("/view/nba.png"));
+        ViewLoader.showStage(getTeam(), "/view/PlayersView.fxml", "Players", stage2);
+    }
+}
+
---
+++ controller/SeasonController.java
@@ -0,0 +1,67 @@
+package controller;
+
+import au.edu.uts.ap.javafx.ViewLoader;
+import au.edu.uts.ap.javafx.Controller;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import model.Season;
+
+public class SeasonController extends Controller<Season> {
+    @FXML private Button roundButton;
+    @FXML private Button currentButton;
+    @FXML private Button gameButton;
+    @FXML private Button resultButton;
+    @FXML private Button closeButton;
+
+    @FXML private void close(ActionEvent event) {
+        stage.close();
+    }
+    public Season getSeason(){
+        return model;
+    }
+
+    @FXML private void viewRounds(ActionEvent event) throws IOException {
+        Stage stage3 = new Stage();
+        stage3.setX(ViewLoader.X + 601);
+        stage3.setY(ViewLoader.Y);
+
+        stage3.getIcons().add(new Image("/view/nba.png"));
+        ViewLoader.showStage(getSeason(), "/view/SeasonRoundView.fxml", "Season Rounds", stage3);
+    }
+
+    @FXML private void viewCurrentRound(ActionEvent event) throws IOException {
+        Stage stage4 = new Stage();
+        stage4.setX(ViewLoader.X + 601);
+        stage4.setY(ViewLoader.Y);
+
+        stage4.getIcons().add(new Image("/view/nba.png"));
+        ViewLoader.showStage(getSeason(), "/view/CurrentRoundTeams.fxml", "Tournament", stage4);
+    }
+
+    /*@FXML private void viewGames(ActionEvent event) throws IOException {
+        Stage stage5 = new Stage();
+        stage5.setX(ViewLoader.X + 601);
+        stage5.setY(ViewLoader.Y);
+
+        stage5.getIcons().add(new Image("/view/nba.png"));
+        ViewLoader.showStage(getSeason(), "/view/SeasonGamesView.fxml", "All Games Played!", stage5);
+    }*/
+
+    @FXML private void viewResults(ActionEvent event) throws IOException {
+        Stage stage6 = new Stage();
+        stage6.setX(ViewLoader.X + 601);
+        stage6.setY(ViewLoader.Y);
+
+        stage6.getIcons().add(new Image("/view/nba.png"));
+        ViewLoader.showStage(getSeason(), "/view/RecordView.fxml", "Season Record", stage6);
+    }
+} 
+
